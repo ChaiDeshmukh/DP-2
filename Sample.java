dd1a3611f20c8a4be85e3baa334fb5971d937911
Chaitrali Deshmukh <42701774+ChaiDeshmukh@users.noreply.github.com>
2020-02-15 14:30:39 -0800

    adding 1st problem

--- Sample.java
+++ Sample.java
@@ -5,3 +5,17 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+Problem 1: Paint House
+
+take min of other two houses an update the array
+
+Time Complexity : O(n) where n -> no of houses
+Space Complexity : O(1)  
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : no
+
+=================================================================
+
+Problem 2: Coin Change II
