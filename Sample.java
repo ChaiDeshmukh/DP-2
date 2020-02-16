348cdd839384bc8733bcaf38e18f9b0d04f14925
Chaitrali Deshmukh <42701774+ChaiDeshmukh@users.noreply.github.com>
2020-02-15 16:30:49 -0800

    adding bruteforce approach

--- Sample.java
+++ Sample.java
@@ -19,3 +19,22 @@ Any problem you faced while coding this : no
 =================================================================
 
 Problem 2: Coin Change II
+
+BruteForce:
+
+either we choose a coin or not choose a coin
+        int case1 = helper(coins, amount - coins[i], i); //if chosen
+        
+        int case2 = helper(coins, amount, i+1); // if not chosen
+return no of ways in the end i.e. case1 + case2
+
+Time Complexity : o(m2^n) 
+Space Complexity : O(n)  
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : no
+
+
+
+Dp Solution:
+
+
